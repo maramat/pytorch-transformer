571cdc6decc4815ab63f580b298373ad6312d621
maramat <35898311+maramat@users.noreply.github.com>
2024-11-30 20:11:31 +0000

    Updaing to have en-cs training

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # pytorch-transformer
 Attention is all you need implementation
+This is copy of following repository, https://github.com/hkproj/pytorch-transformer 
+
 
 YouTube video with full step-by-step implementation: https://www.youtube.com/watch?v=ISNdQcPhsts
 
--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 20,
+        "num_epochs": 30,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "en",
-        "lang_tgt": "it",
+        "lang_tgt": "cs",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
